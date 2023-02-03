d6c84e5e50e76d3749128725a15c5a34b8a68e14
George <gwmkwan@gmail.com>
2023-02-02 17:20:42 -0700

    feat: add geometries with depth and jaggedness

--- main.js
+++ main.js
@@ -20,11 +20,21 @@ const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
 const planeMaterial = new THREE.MeshPhongMaterial({
   color: 0xff0000,
   side: THREE.DoubleSide,
+  flatShading: true,
 });
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
 scene.add(planeMesh);
 
-console.log(planeGeometry);
+console.log(planeMesh.geometry.attributes.position.array);
+
+const { array } = planeMesh.geometry.attributes.position;
+for (let i = 0; i < array.length; i += 3) {
+  const x = array[i];
+  const y = array[i + 1];
+  const z = array[i + 2];
+
+  array[i + 2] = z + Math.random();
+}
 
 const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(0, 0, 1);
