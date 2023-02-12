bdca5da0669e121fe15a81cfb9e86ca89637e31c
George <gwmkwan@gmail.com>
2023-02-11 21:54:16 -0700

    feat: add maintain screen aspect ratio when resize

--- main.js
+++ main.js
@@ -262,3 +262,9 @@ document.querySelector('#viewWorkBtn').addEventListener('click', (e) => {
     delay: 2,
   });
 });
+
+addEventListener('resize', () => {
+  camera.aspect = innerWidth / innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(innerWidth, innerHeight);
+});
